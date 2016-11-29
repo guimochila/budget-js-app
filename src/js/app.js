e274d79a6f03e9f44197d7c1b09bb6cff574d7f8
Guilherme Scaldelai <gui.mochila@gmail.com>
2016-11-29 23:48:11 +0000

    ESLint added to the project. Starting breaking down the app.js into modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,140 +1,6 @@
 'use strict';
 
-// Budget Controller Module
-var budgetController = (function () {
-
-    var Expense = function (id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-        this.percentage = -1;
-    };
-
-    Expense.prototype.calcPercentage = function (totalIncome) {
-        if (totalIncome > 0) {
-            this.percentage = Math.round((this.value / totalIncome) * 100);
-        } else {
-            this.percentage = -1;
-        }
-    };
-
-    Expense.prototype.getPercentage = function () {
-        return this.percentage;
-    }
-
-    var Income = function (id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
-
-    var allExpensives = [];
-    var allIncomes = [];
-    var totalExpress = 0;
-
-    var calculateTotal = function (type) {
-        var sum = 0;
-
-        data.allItems[type].forEach(function (cur) {
-            sum += cur.value;
-        });
-        data.total[type] = sum;
-    };
-
-    var data = {
-        allItems: {
-            exp: [],
-            inc: []
-        },
-        total: {
-            exp: 0,
-            inc: 0
-        },
-        budget: 0,
-        percentage: -1
-    };
-
-    return {
-        addItem: function (type, des, val) {
-            var newItem, ID;
-
-            //Create new ID
-            if (data.allItems[type].length > 0) {
-                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-            } else {
-                ID = 0;
-            }
-
-            //Create newItem based on exp or inc type
-            if (type === 'exp') {
-                newItem = new Expense(ID, des, val);
-            } else if (type === 'inc') {
-                newItem = new Income(ID, des, val);
-            }
-
-            // Push in into the data sctructure
-            data.allItems[type].push(newItem);
-
-            // Return the new element
-            return newItem;
-        },
-
-        deleteItem: function (type, id) {
-            var ids, index;
-
-            ids = data.allItems[type].map(function (current) {
-                return current.id;
-            });
-
-            index = ids.indexOf(id);
-
-            if (index !== -1) {
-                data.allItems[type].splice(index, 1);
-            }
-
-        },
-
-        calculateBudget: function () {
-            // Calculate total income and expenses
-            calculateTotal('inc');
-            calculateTotal('exp');
-
-            // Calculate the budget
-            data.budget = data.total.inc - data.total.exp;
-
-            // Calculate the percentage
-            if (data.total.inc > 0) {
-                data.percentage = Math.round((data.total.exp / data.total.inc) * 100);
-            } else {
-                data.percentage = -1;
-            }
-        },
-
-        calculatePercentages: function () {
-            data.allItems.exp.forEach(function (cur) {
-                cur.calcPercentage(data.total.inc);
-            });
-        },
-
-        getPercentage: function () {
-            var allPerc = data.allItems.exp.map(function (cur) {
-                return cur.getPercentage();
-            });
-            return allPerc;
-        },
-
-        getBudget: function () {
-            return {
-                budget: data.budget,
-                totalInc: data.total.inc,
-                totalExp: data.total.exp,
-                percentage: data.percentage
-            }
-        }
-
-    };
-
-})();
+var budgetController = require('./components/budgetController');
 
 // User-interface controller
 var UIController = (function () {
@@ -183,7 +49,7 @@ var UIController = (function () {
                 type: DOMStrings.$inputType.value, // inc or exp
                 description: DOMStrings.$inputDescription.value,
                 value: parseFloat(DOMStrings.$inputValue.value)
-            }
+            };
         },
 
         addListItem: function (obj, type) {
@@ -222,7 +88,7 @@ var UIController = (function () {
 
             fieldsArr = Array.prototype.slice.call(fields);
             fieldsArr.forEach(function (element, index, arr) {
-                element.value = "";
+                element.value = '';
             }, this);
 
             fieldsArr[0].focus();
@@ -334,7 +200,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 1. Get input field data
         input = UICtrl.getinput();
 
-        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+        if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
             // 2. Add item to budget controller
             newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
