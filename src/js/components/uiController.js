d32e893294b6033f2d35d1e863e0bb21fb303ba0
Guilherme Scaldelai <gui.mochila@gmail.com>
2016-12-01 22:10:16 +0000

    Separating public functions from privates on UI.

--- src/js/components/uiController.js
+++ src/js/components/uiController.js
@@ -16,11 +16,12 @@ var UIController = (function () {
         $container: document.querySelector('.container'),
     };
 
-   function nodeListForEach (list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
+    // Private functions
+    function nodeListForEach(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
 
     function formatNumber(num, type) {
         var numSplit, int, dec;
@@ -36,119 +37,130 @@ var UIController = (function () {
 
         dec = numSplit[1];
 
-        return (type === 'exp' ? '-' : '+') + ' ' + int + '.' +  dec;
+        return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+    }
+
+    // Public functions
+    function getinput() {
+        return {
+            type: DOMStrings.$inputType.value, // inc or exp
+            description: DOMStrings.$inputDescription.value,
+            value: parseFloat(DOMStrings.$inputValue.value)
+        };
+    }
+
+    function addListItem(obj, type) {
+        var html, newHtml, element;
+        // Create HTML string with placehold text
+
+        if (type === 'inc') {
+            element = DOMStrings.$incomeContainer;
+            html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description"> %description% </div ><div class="right clearfix"><div class="item__value"> %value% </div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        } else if (type === 'exp') {
+            element = DOMStrings.$expenseContainer;
+            html = '<div class="item clearfix" id="exp-%id%"> <div class="item__description"> %description%</div><div class="right clearfix"> <div class="item__value"> %value% </div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div></div>';
+        }
+
+        // Replace the placeholder text with data
+        newHtml = html.replace('%id%', obj.id);
+        newHtml = newHtml.replace('%description%', obj.description);
+        newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
+
+        // Insert the HTML into the DOM
+        element.insertAdjacentHTML('beforeend', newHtml);
+
+    }
+
+    function deleteListItem(selectorID) {
+
+        var el = document.getElementById(selectorID);
+        el.parentNode.removeChild(el);
+
+    };
+
+    function clearFields() {
+        var fields, fieldsArr;
+
+        fields = document.querySelectorAll('.add__description' + ', ' + '.add__value');
+
+        fieldsArr = Array.prototype.slice.call(fields);
+        fieldsArr.forEach(function (element) {
+            element.value = '';
+        }, this);
+
+        fieldsArr[0].focus();
+    }
+
+    function displayBudget(obj) {
+        var type;
+
+        obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+        DOMStrings.$budgetLabel.textContent = formatNumber(obj.budget, type);
+        DOMStrings.$incomeLabel.textContent = formatNumber(obj.totalInc, 'inc');
+        DOMStrings.$expenseLabel.textContent = formatNumber(obj.totalExp, 'exp');
+
+        if (obj.percentage > 0) {
+            DOMStrings.$percentageLabel.textContent = obj.percentage + '%';
+        } else {
+            DOMStrings.$percentageLabel.textContent = '---';
+        }
+    }
+
+    function displayPercentage(percentages) {
+
+        var fields = document.querySelectorAll('.item__percentage');
+
+        var nodeListForEach = function (list, callback) {
+            for (var i = 0; i < list.length; i++) {
+                callback(list[i], i);
+            }
+        };
+
+        nodeListForEach(fields, function (current, index) {
+            if (percentages[index] > 0) {
+                current.textContent = percentages[index] + '%';
+            } else {
+                current.textContent = '---';
+            }
+        });
+    }
+
+    function displayMonth() {
+        var now, year, month, months;
+
+        now = new Date();
+        year = now.getFullYear();
+
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        month = now.getMonth();
+        document.querySelector('.budget__title--month').textContent = months[month] + ' ' + year;
+    }
+
+    function changedType() {
+        var fields = document.querySelectorAll('.add__type , .add__description, .add__value');
+
+        nodeListForEach(fields, function (item) {
+            item.classList.toggle('red-focus');
+        });
+
+        DOMStrings.$inputBtn.classList.toggle('red');
+    }
+
+    function getDOMStrings() {
+        return DOMStrings;
     }
 
     return {
-        getinput: function () {
-            return {
-                type: DOMStrings.$inputType.value, // inc or exp
-                description: DOMStrings.$inputDescription.value,
-                value: parseFloat(DOMStrings.$inputValue.value)
-            };
-        },
-
-        addListItem: function (obj, type) {
-            var html, newHtml, element;
-            // Create HTML string with placehold text
-
-            if (type === 'inc') {
-                element = DOMStrings.$incomeContainer;
-                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description"> %description% </div ><div class="right clearfix"><div class="item__value"> %value% </div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
-            } else if (type === 'exp') {
-                element = DOMStrings.$expenseContainer;
-                html = '<div class="item clearfix" id="exp-%id%"> <div class="item__description"> %description%</div><div class="right clearfix"> <div class="item__value"> %value% </div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div></div>';
-            }
-
-            // Replace the placeholder text with data
-            newHtml = html.replace('%id%', obj.id);
-            newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
-
-            // Insert the HTML into the DOM
-            element.insertAdjacentHTML('beforeend', newHtml);
-
-        },
-
-        deleteListItem: function (selectorID) {
-
-            var el = document.getElementById(selectorID);
-            el.parentNode.removeChild(el);
-
-        },
-
-        clearFields: function () {
-            var fields, fieldsArr;
-
-            fields = document.querySelectorAll('.add__description' + ', ' + '.add__value');
-
-            fieldsArr = Array.prototype.slice.call(fields);
-            fieldsArr.forEach(function (element) {
-                element.value = '';
-            }, this);
-
-            fieldsArr[0].focus();
-        },
-
-        displayBudget: function (obj) {
-            var type;
-
-            obj.budget > 0 ? type = 'inc' : type = 'exp';
-
-            DOMStrings.$budgetLabel.textContent = formatNumber(obj.budget, type);
-            DOMStrings.$incomeLabel.textContent = formatNumber(obj.totalInc, 'inc');
-            DOMStrings.$expenseLabel.textContent = formatNumber(obj.totalExp, 'exp');
-
-            if (obj.percentage > 0) {
-                DOMStrings.$percentageLabel.textContent = obj.percentage + '%';
-            } else {
-                DOMStrings.$percentageLabel.textContent = '---';
-            }
-        },
-
-        displayPercentage: function (percentages) {
-
-            var fields = document.querySelectorAll('.item__percentage');
-
-            var nodeListForEach = function (list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
-            nodeListForEach(fields, function (current, index) {
-                if (percentages[index] > 0) {
-                    current.textContent = percentages[index] + '%';
-                } else {
-                    current.textContent = '---';
-                }
-            });
-        },
-
-        displayMonth: function() {
-            var now, year, month, months;
-
-            now = new Date();
-            year = now.getFullYear();
-
-            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-            month =  now.getMonth();
-            document.querySelector('.budget__title--month').textContent = months[month] + ' ' + year;
-        },
-
-        changedType: function () {
-            var fields = document.querySelectorAll('.add__type , .add__description, .add__value');
-
-            nodeListForEach(fields, function (item) {
-                item.classList.toggle('red-focus');
-            });
-
-            DOMStrings.$inputBtn.classList.toggle('red');
-        },
-
-        getDOMStrings: function () {
-            return DOMStrings;
-        }
+        getinput:       getinput,
+        addListItem:    addListItem,
+        deleteListItem: deleteListItem,
+        clearFields:    clearFields,
+        displayBudget:  displayBudget,
+        displayPercentage: displayPercentage,
+        displayMonth:   displayMonth,
+        changedType:    changedType,
+        getDOMStrings:  getDOMStrings
     };
 
 })();
