e18a8431676865a4a7015ce6048e51841e5cd2b0
Guilherme Scaldelai <gui.mochila@gmail.com>
2016-12-03 22:57:26 +0000

    More uni tests added.

--- jasmine/spec/BudgetControllerSpec.js
+++ jasmine/spec/BudgetControllerSpec.js
@@ -1,18 +1,49 @@
-describe('Budget Controller', function() {
+describe('Budget Controller', function () {
 
-    var addItem = budgetController.addItem;
+    var b = budgetController;
 
-    it('should be able to add item', function () {
-        addItem('exp', 'Test', 100);
-        budgetController.calculateBudget();
+    // Testing adding expense
+    it('should be able to add expense', function () {
+        b.addItem('exp', 'Test', 100);
+        b.calculateBudget();
 
-        var result = budgetController.getBudget();
+        var result = b.getBudget();
 
         expect(result).toEqual(jasmine.objectContaining({
             budget: -100,
             totalInc: 0,
             totalExp: 100,
             percentage: -1
+        }));
+    });
+
+    // Testing remove items
+    it('should be able to remove expense', function () {
+        b.deleteItem('exp', 0);
+        b.calculateBudget();
+
+        var result = b.getBudget();
+
+        expect(result).toEqual(jasmine.objectContaining({
+            budget: 0,
+            totalInc: 0,
+            totalExp: 0,
+            percentage: -1
+        }));
+    });
+
+    // Testing adding incoming
+    it('should be able to add incoming', function () {
+        b.addItem('inc', 'Test', 100);
+        b.calculateBudget();
+
+        var result = b.getBudget();
+
+        expect(result).toEqual(jasmine.objectContaining({
+            budget: 100,
+            totalInc: 100,
+            totalExp: 0,
+            percentage: 0
         }));
     });
 });
